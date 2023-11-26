c14d2199155a14d275a8871e6cf918ca5e8f19a3
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:44:53 +0200

    mm2

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,6 +1,10 @@
+import template1 from "../../../public/images/img/template1.png";
+
+
 import AnimatedHeading from "@/components/AnimatedHeading";
 import TransitionPageEffect from "@/components/TransitionPageEffect";
 import Head from "next/head";
+import FeatureProject from "./ProjectComponents/FeatureProject";
 const Projects = () => {
     return (
         <>
@@ -15,7 +19,20 @@ const Projects = () => {
                     className="animate-heading text-center  xl:text-5xl
             md:text-3xl"
                 />
-                this main
+                                <div className="grid grid-cols-12 gap-x-[10px]">
+                    {/*start 1st section */}
+                    <div className=" col-span-12">
+                        <FeatureProject
+                            link="https://mo634.github.io/Template1/"
+                            repoLink={"https://github.com/mo634/Template1"}
+                            image={template1}
+                            title={"Template 1 "}
+                            head="website template "
+                            summary=" designed by using Pure  HTML CSS With FontAwseome "
+                        />
+                    </div>
+                    {/*end 1st section */}
+                    </div>
                 </main>
         </>
     );
