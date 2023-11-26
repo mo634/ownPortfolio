10a7ec7565526b564522496b934f0d78a6d0a0ad
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:36:23 +0200

    m4

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,17 +1,4 @@
-import AnimatedHeading from "@/components/AnimatedHeading";
 import Head from "next/head";
-import React from "react";
-import FeatureProject from "./ProjectComponents/FeatureProject";
-import template1 from "../../../public/images/img/template1.png";
-import template2 from "../../../public/images/img/template2.png";
-import template3 from "../../../public/images/img/template3.png";
-import template4 from "../../../public/images/img/template4.png";
-import template5 from "../../../public/images/img/Template5.png";
-import prayeTimes from "../../../public/images/img/praye-times.png";
-import loginform from "../../../public/images/projects/loginform.png";
-import aiSumm from "../../../public/images/img/Ai-summarizer.png";
-import WebsiteProject from "./ProjectComponents/WebsiteProject";
-import TransitionPageEffect from "@/components/TransitionPageEffect";
 const Projects = () => {
     return (
         <>
@@ -23,4 +10,4 @@ const Projects = () => {
     );
 };
 
-export default Projects;
+export default Projects
