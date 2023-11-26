f55d085d11a72aad5b8fe690fb9a6d9c1a0472f4
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:27:51 +0200

    complete project section

---
+++ src/pages/projects/ProjectComponents/WebsiteProject.jsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {GithubIcon} from "@/components/Icons";
+const WebsiteProject = ({image, link, title, head,repoLink}) => {
+    return (
+        <article className="h-[90%] mb-[50px] dark:bg-dark dark:border-white relative flex flex-col gap-y-[20px] p-[20px] rounded-2xl bg-light shadow-md border-solid border-[5px] border-black">
+            <div className="dark:bg-white left-0 w-[101%]  absolute h-full bg-dark rounded-2xl -z-10 top-2 right-[-10px]"></div>
+
+            {/* top side  */}
+
+            <Link target="_blank" href={link} className="w-1/2 overflow-hidden rounded-2xl w-full">
+                <Image
+                                        priority
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    src={image} alt="not Found" className="hover:scale-[1.2] hover:-rotate-1 duration-[0.5s]" />
+            </Link>
+
+            {/* bottom side  */}
+
+            <div className="md:pl-[2px] flex flex-col justify-center pl-5 w-full">
+                <span className="text-primary dark:text-primaryDark ">{title}</span>
+                <Link href={"#"}>
+                    <h2 className="font-bold text-3xl  hover:cursor-pointer hover:underline ">{head}</h2>
+                </Link>
+                <div className="flex items-center justify-between mt-[20px]">
+                    <Link target="_blank" href={repoLink} className=" hover:underline text-[20px]">
+                        Visit github repo
+                    </Link>
+                    <Link target="_blank" href={"https://github.com/mo634"} className="">
+                        <GithubIcon className={"w-10 mr-[10px]"} />
+                    </Link>
+                </div>
+            </div>
+        </article>
+    );
+};
+
+export default WebsiteProject;
---
+++ src/pages/projects/index.jsx
@@ -0,0 +1,126 @@
+import AnimatedHeading from "@/components/AnimatedHeading";
+import Head from "next/head";
+import React from "react";
+import FeatureProject from "./ProjectComponents/FeatureProject";
+import template1 from "../../../public/images/img/template1.png";
+import template2 from "../../../public/images/img/template2.png";
+import template3 from "../../../public/images/img/template3.png";
+import template4 from "../../../public/images/img/template4.png";
+import template5 from "../../../public/images/img/Template5.png";
+import prayeTimes from "../../../public/images/img/praye-times.png";
+import loginform from "../../../public/images/projects/loginform.png";
+import aiSumm from "../../../public/images/img/Ai-summarizer.png";
+import WebsiteProject from "./ProjectComponents/WebsiteProject";
+import TransitionPageEffect from "@/components/TransitionPageEffect";
+const Projects = () => {
+    return (
+        <>
+            <Head>
+                <title>Project Page</title>
+                <meta name="description" content="This is Page of My Projects" />
+            </Head>
+            <TransitionPageEffect />
+            <main className=" py-[20px] xl:py-[50px] wrapper flex flex-col justify-center items-center mb-10">
+                <AnimatedHeading
+                    text="Imagination Trumps Knowledge!"
+                    className="animate-heading text-center  xl:text-5xl
+            md:text-3xl"
+                />
+                <div className="grid grid-cols-12 gap-x-[10px]">
+                    {/*start 1st section */}
+                    <div className=" col-span-12">
+                        <FeatureProject
+                            link="https://mo634.github.io/Template1/"
+                            repoLink={"https://github.com/mo634/Template1"}
+                            image={template1}
+                            title={"Template 1 "}
+                            head="website template "
+                            summary=" designed by using Pure  HTML CSS With FontAwseome "
+                        />
+                    </div>
+                    {/*end 1st section */}
+
+                    {/*start 2nd section */}
+                    <div className=" col-span-6 xl:col-span-12 ">
+                        <WebsiteProject
+                            link="https://mo634.github.io/Template2/"
+                            repoLink="https://github.com/mo634/Template2"
+                            image={template2}
+                            title={"website template "}
+                            head=" designed by using Pure  HTML CSS With FontAwseome "
+                        />
+                    </div>
+
+
+                    <div className=" col-span-6 xl:col-span-12">
+                    <WebsiteProject
+                            link="https://mo634.github.io/Template3/"
+                            repoLink="https://github.com/mo634/Template3"
+                            image={template3}
+                            title={"website template "}
+                            head=" designed by using Pure  HTML CSS With FontAwseome "
+                        />
+                    </div>
+                    {/*end 2nd section */}
+
+                    {/*start 3rd section */}
+                    <div className=" col-span-12">
+                    <FeatureProject
+                            link="https://mo634.github.io/Template4/"
+                            repoLink="https://github.com/mo634/Template4"
+                            image={template4}
+                            title={"Template 4 "}
+                            head="website sss template "
+                            summary=" designed by using Pure  HTML CSS With FontAwseome "
+                        />
+                    </div>
+                    {/*end 3rd section */}
+                    <div className=" col-span-6 xl:col-span-12">
+                        <WebsiteProject
+                            link="https://mo634.github.io/template5/"
+                            repoLink="https://github.com/mo634/template5"
+                            image={template5}
+                            title={"Template Website"}
+                            head="designed by using pure HTML , CSS , JS "
+                        />
+                    </div>
+                    {/*start 4th section */}
+                    <div className=" col-span-6 xl:col-span-12">
+                        <WebsiteProject
+                            link="https://mo634.github.io/prayer-times/"
+                            repoLink="https://github.com/mo634/prayer-times"
+                            image={prayeTimes}
+                            title={"Preyer Times For Muslims"}
+                            head="designed by using react frame work and Material UI"
+                        />
+                    </div>
+                    <div className=" col-span-12">
+                        <FeatureProject
+                            link="https://mo634.github.io/ai-summarizer/"
+                            repoLink={"https://github.com/mo634/ai-summarizer"}
+                            image={aiSumm}
+                            title={"AI article summarizer "}
+                            head="for summarize aricles by providing the link of article "
+                            summary=" designed by using react , tailwind css , redux toolkit api query  "
+                        />
+                    </div>
+                    {/*end 4th section */}
+
+                    {/* login project  */}
+                    <div className=" col-span-12">
+                        <FeatureProject
+                            link="https://next-auth-login-iota.vercel.app/"
+                            repoLink={"https://github.com/mo634/next-auth-login"}
+                            image={loginform}
+                            title={"Simple login form  "}
+                            head="you can register or login if have already account "
+                            summary="designed using mongodb , Next-auth , to handle login , register and user authentication as the user info after register it inserted  to monogdb and then it validate when login to autharize user"
+                        />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default Projects;
