44ef799845028524f0e164dd7a2873b6f15dbd4e
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:57:55 +0200

    nb2

--- src/pages/projects/ProjectComponents/FeatureProject.jsx
+++ src/pages/projects/ProjectComponents/FeatureProject.jsx
@@ -11,8 +11,38 @@ const FeatureProject = ({image, link,repoLink, title, head, summary}) => {
                 
             {/* left side  */}
 
+                <Link
+                    target="_blank"
+                    href={link ?? ''} className="xl:w-[100%]  w-1/2 overflow-hidden rounded-2xl">
+                <Image
+                    priority
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    src={image}
+                    alt="not Found"
+                    className="hover:scale-[1.2] hover:-rotate-1 duration-[0.5s]"
+                />
+            </Link>
 
+            
             {/* right side  */}
+
+            <div className="md:pl-[5px]  w-1/2 flex flex-col justify-center pl-5 xl:w-[100%]">
+                <span className="text-primary dark:text-primaryDark ">{title}</span>
+                <Link href={"#"}>
+                    <h2 className="font-bold text-3xl  hover:cursor-pointer hover:underline ">{head}</h2>
+                </Link>
+                    <p className="text-dark/75 my-3 dark:text-light leading-relaxed">{summary}</p>
+                    
+                {/* icon and btn */}
+                <div className="flex items-center gap-x-[20px]">
+                    <Link href={"#"} className="">
+                        <GithubIcon className={"w-10 mr-[10px]"} />
+                    </Link>
+                    <Link target="_blank" href={repoLink??' '} className="dark:bg-light dark:text-dark bg-dark py-[5px] px-[10px] text-light rounded-md shadow-md">
+                        Visit Project Repo
+                    </Link>
+                </div>
+            </div>
 
 </div>
         </article>
