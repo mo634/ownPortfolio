30953147e148b4a75db6ba0670cdc385836d8556
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:54:19 +0200

    mm32

--- src/pages/projects/ProjectComponents/FeatureProject.jsx
+++ src/pages/projects/ProjectComponents/FeatureProject.jsx
@@ -25,23 +25,6 @@ const FeatureProject = ({image, link,repoLink, title, head, summary}) => {
 
             {/* right side  */}
 
-            <div className="md:pl-[5px]  w-1/2 flex flex-col justify-center pl-5 xl:w-[100%]">
-                <span className="text-primary dark:text-primaryDark ">{title}</span>
-                <Link href={"#"}>
-                    <h2 className="font-bold text-3xl  hover:cursor-pointer hover:underline ">{head}</h2>
-                </Link>
-                    <p className="text-dark/75 my-3 dark:text-light leading-relaxed">{summary}</p>
-                    
-                {/* icon and btn */}
-                <div className="flex items-center gap-x-[20px]">
-                    <Link href={"#"} className="">
-                        <GithubIcon className={"w-10 mr-[10px]"} />
-                    </Link>
-                    <Link target="_blank" href={repoLink} className="dark:bg-light dark:text-dark bg-dark py-[5px] px-[10px] text-light rounded-md shadow-md">
-                        Visit Project Repo
-                    </Link>
-                </div>
-            </div>
 </div>
         </article>
     );
