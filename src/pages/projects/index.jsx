4bb2d0a6a1e03852ee8776f89272203b9211a20b
MohamedMostafa <mido221gyefue@gmail.com>
2023-11-26 20:37:51 +0200

    m5

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -6,6 +6,15 @@ const Projects = () => {
                 <title>Project Page</title>
                 <meta name="description" content="This is Page of My Projects" />
             </Head>
+            <TransitionPageEffect />
+            <main className=" py-[20px] xl:py-[50px] wrapper flex flex-col justify-center items-center mb-10">
+                <AnimatedHeading
+                    text="Imagination Trumps Knowledge!"
+                    className="animate-heading text-center  xl:text-5xl
+            md:text-3xl"
+                />
+                this main
+                </main>
         </>
     );
 };
